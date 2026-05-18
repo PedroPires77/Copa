13e07acf9400c5823b3efb61dfe2ddc2d267ee75
PedroPires77 <pedro90482386@edu.unipar.br>
2026-05-18 20:07:58 -0300

    Identificação do dia atual

--- unicopa-master/App.js
+++ unicopa-master/App.js
@@ -1,40 +1,40 @@
 import { StyleSheet, Text, View, Image, ImageBackground, SectionList } from 'react-native';
 import GameCard from './components/GameCard';
-import dados from './assets/dados.json'
+import dados from './assets/dados.json';
 
 export default function App() {
 
-  const jogos = dados.jogos
+  const jogos = dados.jogos;
+
+  const hoje = new Date();
+  const hojeStr = `${hoje.getFullYear()}-${String(hoje.getMonth() + 1).padStart(2, '0')}-${String(hoje.getDate()).padStart(2, '0')}`;
 
   const agruparPorData = (jogos) => {
     return jogos.reduce((acc, jogo) => {
-
-      const data = jogo.data_brasilia
-
+      const data = jogo.data_brasilia;
       if (!acc[data]) {
-        acc[data] = []
-      } 
+        acc[data] = [];
+      }
+      acc[data].push(jogo);
+      return acc;
+    }, {});
+  };
 
-      acc[data].push(jogo)
+  const jogosAgrupados = agruparPorData(jogos);
 
-      return acc
-
-    }, {})
-  }
-
-  const jogosAgrupados = agruparPorData(jogos)
-
-  const jogosTratados = Object.keys(jogosAgrupados).map(data => {
-    return {
-      title: data,
-      data: jogosAgrupados[data]
-    }
-  })
+  const jogosTratados = Object.keys(jogosAgrupados).map(data => ({
+    title: data,
+    data: jogosAgrupados[data],
+    isHoje: data === hojeStr,
+  }));
 
   return (
-    <ImageBackground style={styles.container}
-      source={require('./assets/bg-overlay.png')}>
-      <Image style={styles.logo}
+    <ImageBackground
+      style={styles.container}
+      source={require('./assets/bg-overlay.png')}
+    >
+      <Image
+        style={styles.logo}
         source={require('./assets/unicopa.png')}
       />
 
@@ -45,24 +45,23 @@ export default function App() {
         keyExtractor={(item, index) => item + index}
         renderItem={() => null}
         renderSectionHeader={({ section }) => (
-          <View style={styles.card} >
-
-            <Text style={styles.data}> {section.title} </Text>
-              {
-                section.data.map((jogo) => (
-                  <GameCard key={jogo.id} game={jogo} />
-                ))
-              }
-
-
-
+          <View style={[styles.card, section.isHoje && styles.cardHoje]}>
+            <View style={styles.dataContainer}>
+              <Text style={[styles.data, section.isHoje && styles.dataHoje]}>
+                {section.title}
+              </Text>
+              {section.isHoje && (
+                <View style={styles.badgeHoje}>
+                  <Text style={styles.badgeHojeTexto}>HOJE</Text>
+                </View>
+              )}
+            </View>
+            {section.data.map((jogo) => (
+              <GameCard key={jogo.id} game={jogo} />
+            ))}
           </View>
-        )
-        }
-
-
+        )}
       />
-
     </ImageBackground>
   );
 }
@@ -78,7 +77,7 @@ const styles = StyleSheet.create({
     marginTop: 20,
     width: 200,
     height: 50,
-    resizeMode: 'contain'
+    resizeMode: 'contain',
   },
   title: {
     marginTop: 10,
@@ -93,17 +92,34 @@ const styles = StyleSheet.create({
     borderRadius: 12,
     padding: 15,
   },
+  cardHoje: {
+    borderWidth: 2,
+    borderColor: '#f2cc2f',
+    backgroundColor: '#111f2e',
+  },
+  dataContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+    gap: 8,
+  },
   data: {
     color: '#f2cc2f',
     fontSize: 22,
     fontWeight: 'bold',
-    marginBottom: 10
   },
-
-  jogo: {
-    marginBottom: 20,
-    borderBottomWidth: 1,
-    borderBottomColor: '#1e2d3d',
-    paddingBottom: 15
+  dataHoje: {
+    color: '#f2cc2f',
+  },
+  badgeHoje: {
+    backgroundColor: '#f2cc2f',
+    borderRadius: 6,
+    paddingHorizontal: 8,
+    paddingVertical: 2,
+  },
+  badgeHojeTexto: {
+    color: '#040b13',
+    fontSize: 11,
+    fontWeight: 'bold',
   },
 });
